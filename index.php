d058341f9a45d6629f2bca48be71ed34e3b29d8c
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-09-26 16:26:52 +0200

    feat table

--- index.php
+++ index.php
@@ -53,14 +53,65 @@ var_dump($hotels);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
 
-    <!--BOOTSTRAP
+    BOOTSTRAP
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
--->
+
 </head>
 
 <body>
 
+    <h3>
+
+        <?php
+
+        $key = array_keys($hotels);
+
+        var_dump($key);
+
+        foreach ($hotels as $key => $hotel) {
+
+            //$key = 0, 1, 2, etc..
+        
+            echo "Key : " . $key . "<br>";
+            foreach ($hotel as $key_description => $info) {
+
+                //$key_description = name, description, parking etc..
+                //$info = "stringhe";
+        
+                echo $key_description . ": " . $info . "<br>";
+
+            }
+            echo "<hr>";
+        }
+        ?>
+    </h3>
+
+
+    <table class="table">
+        <?php foreach ($hotels as $key => $hotel):
+            foreach ($hotel as $key_description => $info): ?>
+                <thead>
+                    <tr>
+
+                        <th scope="col">
+                            <?php echo $key_description; ?>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">
+                            <?php echo $key; ?>
+                        </th>
+                        <td>
+                            <?php echo $info; ?>
+                        </td>
+                    </tr>
+
+                </tbody>
+            <?php endforeach; endforeach; ?>
+    </table>
 
 
 </body>
